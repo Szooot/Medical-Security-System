2d8bd37032ecde4329ed080ccece3f4b8e3d3a4b
Michal Szot <michalos277@gmail.com>
2024-05-13 22:51:16 +0200

    Styling components

---
+++ README.md
@@ -0,0 +1,45 @@
+# Medical Security System
+
+Medical Security System is a simple app, where all informations about patients are stored in local blockchain. Users are stored
+on local database, where they can keep informations about their blocks and in case of that, they don't have to remeber their block id's.
+
+## Installation
+
+### Requirements
+
+Before app installation make sure, You have installed tools like:
+
+- [Python](https://www.python.org/downloads/) (Python 3.x)
+- [PostgreSQL](https://www.postgresql.org/download/) (Database)
+
+### Repository Cloning
+
+```bash
+$ git clone https://github.com/Szoot/Medical-Security-System.git
+$ cd Medical-Security-System
+```
+### Dependency Installation
+
+```bash
+$ python -m venv venv
+$ source venv/bin/activate      # Unix/Linux
+$ venv\Scripts\activate         # Windows
+$ pip install -r requirements.txt
+```
+
+### Database Configuration
+
+Change username, password, database_name to your local database config.
+
+```bash
+DATABASE_URL=postgresql://username:password@localhost/database_name
+```
+
+### Launching The Application
+
+Application will be available at `http://localhost:5000/`
+
+```bash
+$ flask run
+```
+
--- static/index.js
+++ static/index.js
@@ -13,22 +13,44 @@ function index() {
 
 // Elementy do kontrolowania modalu
 const signInButton = document.getElementById('signInButton');
+
 const loginModal = document.getElementById('loginModal');
-const closeButton = loginModal.querySelector('.close-button');
+const closeButton1 = loginModal.querySelector('.close-button');
+
+const registerButton = document.getElementById('register');
+const registerModal = document.getElementById('registerModal');
+const closeButton2 = registerModal.querySelector('.close-button');
 
 // Pokazywanie modalu po kliknięciu
-signInButton.addEventListener('click', function() {
+signInButton.addEventListener('click', () => {
   loginModal.style.display = 'flex'; // Ustawienie 'flex' pokazuje modal
 });
 
 // Ukrywanie modalu po kliknięciu przycisku zamykania
-closeButton.addEventListener('click', function() {
+closeButton1.addEventListener('click', () => {
   loginModal.style.display = 'none'; // Ukrycie modalu
 });
 
 // Ukrywanie modalu po kliknięciu poza modalem
-window.addEventListener('click', function(event) {
+window.addEventListener('click', (event) => {
   if (event.target === loginModal) {
     loginModal.style.display = 'none'; // Ukrycie modalu
   }
+});
+
+registerButton.addEventListener('click', () => {
+  loginModal.style.display = 'none';
+  registerModal.style.display = 'flex';
+})
+
+
+window.addEventListener('click', (event) => {
+  if (event.target === registerModal) {
+    registerModal.style.display = 'none'; 
+  }
+});
+
+// Ukrywanie modalu po kliknięciu przycisku zamykania
+closeButton2.addEventListener('click', () => {
+  registerModal.style.display = 'none'; // Ukrycie modalu
 });
